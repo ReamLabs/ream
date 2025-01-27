121bba259dd5c4a1da12a6b664f7250b82514157
unnawut <unnawut@unnawut.com>
2025-01-27 17:22:35 +0700

    refactor: replace c-kzg-4844 with rust-kzg

--- crates/common/consensus/src/kzg_commitment.rs
+++ crates/common/consensus/src/kzg_commitment.rs
@@ -5,7 +5,7 @@ use std::{
 };
 
 use alloy_primitives::hex;
-use c_kzg::BYTES_PER_COMMITMENT;
+use kzg::eip_4844::BYTES_PER_COMMITMENT;
 use ethereum_hashing::hash_fixed;
 use serde::{
     de::{Deserialize, Deserializer},
@@ -28,13 +28,15 @@ impl KzgCommitment {
     }
 
     pub fn empty_for_testing() -> Self {
-        KzgCommitment([0; c_kzg::BYTES_PER_COMMITMENT])
+        KzgCommitment([0; BYTES_PER_COMMITMENT])
     }
 }
 
-impl From<KzgCommitment> for c_kzg::Bytes48 {
+impl From<KzgCommitment> for kzg::eth::c_bindings::Bytes48 {
     fn from(value: KzgCommitment) -> Self {
-        value.0.into()
+        kzg::eth::c_bindings::Bytes48 {
+            bytes: value.0
+        }
     }
 }
 
