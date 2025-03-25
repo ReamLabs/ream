ab43b74b87bcf0359c2966118f0fe11fb0134219
Vid Kersic <vid.kersic@yahoo.com>
2025-03-25 14:18:15 +0100

    feat: add p2p config to cli

--- crates/networking/discv5/src/config.rs
+++ crates/networking/discv5/src/config.rs
@@ -1,9 +1,15 @@
+use std::net::IpAddr;
+
 use discv5::Enr;
 
 pub struct NetworkConfig {
     pub discv5_config: discv5::Config,
 
     pub bootnodes: Vec<Enr>,
+
+    pub addr: IpAddr,
+
+    pub port: u16,
 
     pub disable_discovery: bool,
 
