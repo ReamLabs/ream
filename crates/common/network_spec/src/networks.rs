aa8dcbdab780452022cfa855c4b81bb2699aa3db
aditya172926 <aditya26sg@gmail.com>
2025-04-05 16:05:52 +0530

    update network spec with deposit contract

--- crates/common/network_spec/src/networks.rs
+++ crates/common/network_spec/src/networks.rs
@@ -1,6 +1,6 @@
 use std::sync::{Arc, LazyLock};
 
-use alloy_primitives::{b256, fixed_bytes};
+use alloy_primitives::{address, b256, fixed_bytes, Address};
 use ream_consensus::genesis::Genesis;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -16,6 +16,8 @@ pub enum Network {
 pub struct NetworkSpec {
     pub network: Network,
     pub genesis: Genesis,
+    pub chain_id: u64,
+    pub deposit_contract_address: Address
 }
 
 pub static MAINNET: LazyLock<Arc<NetworkSpec>> = LazyLock::new(|| {
@@ -28,6 +30,8 @@ pub static MAINNET: LazyLock<Arc<NetworkSpec>> = LazyLock::new(|| {
             ),
             genesis_fork_version: fixed_bytes!("0x00000000"),
         },
+        chain_id: 1,
+        deposit_contract_address: address!("0x00000000219ab540356cBB839Cbe05303d7705Fa")
     }
     .into()
 });
@@ -42,6 +46,8 @@ pub static HOLESKY: LazyLock<Arc<NetworkSpec>> = LazyLock::new(|| {
             ),
             genesis_fork_version: fixed_bytes!("0x01017000"),
         },
+        chain_id: 17000,
+        deposit_contract_address: address!("0x4242424242424242424242424242424242424242")
     }
     .into()
 });
@@ -56,6 +62,8 @@ pub static SEPOLIA: LazyLock<Arc<NetworkSpec>> = LazyLock::new(|| {
             ),
             genesis_fork_version: fixed_bytes!("0x90000069"),
         },
+        chain_id: 11155111,
+        deposit_contract_address: address!("0x7f02C3E3c98b133055B8B348B2Ac625669Ed295D")
     }
     .into()
 });
@@ -70,6 +78,8 @@ pub static HOODI: LazyLock<Arc<NetworkSpec>> = LazyLock::new(|| {
             ),
             genesis_fork_version: fixed_bytes!("0x10000910"),
         },
+        chain_id: 560048,
+        deposit_contract_address: address!("0x00000000219ab540356cBB839Cbe05303d7705Fa")
     }
     .into()
 });
@@ -84,6 +94,8 @@ pub static DEV: LazyLock<Arc<NetworkSpec>> = LazyLock::new(|| {
             ),
             genesis_fork_version: fixed_bytes!("0x00000000"),
         },
+        chain_id: 1,
+        deposit_contract_address: address!("0x00000000219ab540356cBB839Cbe05303d7705Fa")
     }
     .into()
 });
