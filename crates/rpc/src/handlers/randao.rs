d9f28e10a22462c4a8d42a935726149ebfca8ff3
varun-doshi <doshivarun202@gmail.com>
2025-04-06 00:11:46 +0530

    fix: use filter for db

--- crates/rpc/src/handlers/randao.rs
+++ crates/rpc/src/handlers/randao.rs
@@ -1,4 +1,4 @@
-use alloy_primitives::FixedBytes;
+use alloy_primitives::B256;
 use ream_storage::db::ReamDB;
 use serde::{Deserialize, Serialize};
 use warp::{
@@ -8,11 +8,11 @@ use warp::{
 };
 
 use super::{BeaconResponse, state::get_state_from_id};
-use crate::types::id::ID;
+use crate::types::{id::ID, query::RandaoQuery};
 
 #[derive(Serialize, Deserialize)]
 struct RandaoResponse {
-    pub randao: FixedBytes<32>,
+    pub randao: B256,
 }
 
 /// Called by `/states/<state_id>/randao` to get the Randao mix of state.
@@ -20,11 +20,11 @@ struct RandaoResponse {
 /// else will fetch randao of the state epoch
 pub async fn get_randao_mix(
     state_id: ID,
-    epoch: Option<u64>,
+    query: RandaoQuery,
     db: ReamDB,
 ) -> Result<impl Reply, Rejection> {
     let state = get_state_from_id(state_id, &db).await?;
-    let randao_mix = match epoch {
+    let randao_mix = match query.epoch {
         Some(epoch) => state.get_randao_mix(epoch),
         None => state.get_randao_mix(state.get_current_epoch()),
     };
--- crates/rpc/src/routes/beacon.rs
+++ crates/rpc/src/routes/beacon.rs
@@ -12,6 +12,7 @@ use warp::{
     reply::Reply,
 };
 
+use super::with_db;
 use crate::{
     handlers::{genesis::get_genesis, randao::get_randao_mix, validator::get_validator_from_state},
     types::{
@@ -26,6 +27,7 @@ pub fn get_beacon_routes(
     db: ReamDB,
 ) -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
     let beacon_base = path("beacon");
+    let db_filter = with_db(db);
 
     let genesis = beacon_base
         .and(path("genesis"))
@@ -34,36 +36,33 @@ pub fn get_beacon_routes(
         .and_then(move || get_genesis(network_spec.genesis.clone()))
         .with(log("genesis"));
 
-    let randao = {
-        let db = db.clone();
-        beacon_base
-            .and(path("states"))
-            .and(param::<ID>())
-            .and(path("randao"))
-            .and(query::<RandaoQuery>())
-            .and(end())
-            .and(get())
-            .and_then(move |state_id: ID, query: RandaoQuery| {
-                let epoch = query.epoch;
-                get_randao_mix(state_id, epoch, db.clone())
-            })
-    };
+    let randao = beacon_base
+        .and(path("states"))
+        .and(param::<ID>())
+        .and(path("randao"))
+        .and(query::<RandaoQuery>())
+        .and(end())
+        .and(get())
+        .and(db_filter.clone())
+        .and_then(move |state_id: ID, query: RandaoQuery, db: ReamDB| {
+            get_randao_mix(state_id, query, db)
+        })
+        .with(log("randao"));
 
-    let validator = {
-        let db = db.clone();
-        beacon_base
-            .and(path("states"))
-            .and(param::<ID>())
-            .and(path("validator"))
-            .and(param::<ValidatorID>())
-            .and(end())
-            .and(get())
-            .and_then({
-                move |state_id: ID, validator_id: ValidatorID| {
-                    get_validator_from_state(state_id, validator_id, db.clone())
-                }
-            })
-    };
+    let validator = beacon_base
+        .and(path("states"))
+        .and(param::<ID>())
+        .and(path("validator"))
+        .and(param::<ValidatorID>())
+        .and(end())
+        .and(get())
+        .and(db_filter.clone())
+        .and_then({
+            move |state_id: ID, validator_id: ValidatorID, db: ReamDB| {
+                get_validator_from_state(state_id, validator_id, db)
+            }
+        })
+        .with(log("validator"));
 
     genesis.or(validator).or(randao)
 }
