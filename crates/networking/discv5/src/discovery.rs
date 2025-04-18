842ea7898181da5046a75233e32e18760d17e948
VINEET PANT <10172895+vineetpant@users.noreply.github.com>
2025-04-18 15:55:02 +0530

    Update crates/networking/discv5/src/discovery.rs
    Co-authored-by: Kolby Moroz Liebl <31669092+KolbyML@users.noreply.github.com>

--- crates/networking/discv5/src/discovery.rs
+++ crates/networking/discv5/src/discovery.rs
@@ -73,10 +73,11 @@ impl Discovery {
             .map_err(|err| anyhow!("Failed to convert key: {err:?}"))?;
         let mut enr_builder = Enr::builder();
         // Need this test block to pass port and socket for test purpose
-        #[cfg(test)]
-        {
-            enr_builder.ip(config.socket_address);
-            enr_builder.udp4(config.socket_port);
+        if let (socker_address) = config.socker_address {
+             enr_builder.ip(socker_address)
+        }
+        if let (socker_port) = config.socker_port {
+             enr_builder.udp4(socker_port);
         }
         if let Some(attestation_bytes) = config.subnets.attestation_bytes() {
             enr_builder.add_value::<Bytes>(ATTESTATION_BITFIELD_ENR_KEY, &attestation_bytes);
