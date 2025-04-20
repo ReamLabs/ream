e3cee99b8cb3f9019b2b4040aa61e4b7d7ec67f9
Vid Kersic <vid.kersic@yahoo.com>
2025-04-20 23:47:02 +0200

    chore: improve get blob sidecars

--- crates/common/consensus/src/beacon_block_header.rs
+++ crates/common/consensus/src/beacon_block_header.rs
@@ -2,7 +2,10 @@ use alloy_primitives::B256;
 use ream_bls::BLSSignature;
 use serde::{Deserialize, Serialize};
 use ssz_derive::{Decode, Encode};
+use tree_hash::TreeHash;
 use tree_hash_derive::TreeHash;
+
+use crate::deneb::beacon_block::SignedBeaconBlock;
 
 #[derive(Debug, PartialEq, Clone, Serialize, Deserialize, Encode, Decode, TreeHash)]
 pub struct SignedBeaconBlockHeader {
@@ -17,4 +20,19 @@ pub struct BeaconBlockHeader {
     pub parent_root: B256,
     pub state_root: B256,
     pub body_root: B256,
+}
+
+pub fn compute_signed_block_header(signed_block: SignedBeaconBlock) -> SignedBeaconBlockHeader {
+    let block = signed_block.message;
+    let block_header = BeaconBlockHeader {
+        slot: block.slot,
+        proposer_index: block.proposer_index,
+        parent_root: block.parent_root,
+        state_root: block.state_root,
+        body_root: block.body.tree_hash_root(),
+    };
+    SignedBeaconBlockHeader {
+        message: block_header,
+        signature: signed_block.signature,
+    }
 }
--- crates/rpc/src/handlers/blob_sidecar.rs
+++ crates/rpc/src/handlers/blob_sidecar.rs
@@ -2,16 +2,12 @@ use actix_web::{
     HttpResponse, Responder, get,
     web::{Data, Json, Path},
 };
-use ream_consensus::blob_sidecar::{BlobIdentifier, BlobSidecar};
+use ream_consensus::blob_sidecar::BlobIdentifier;
 use ream_storage::{db::ReamDB, tables::Table};
-use ssz_types::FixedVector;
 use tracing::error;
 
 use crate::{
-    handlers::{
-        block::{get_beacon_block_from_id, get_block_root_from_id},
-        header::get_header_from_slot,
-    },
+    handlers::block::{get_beacon_block_from_id, get_block_root_from_id},
     types::{errors::ApiError, id::ID, query::BlobSidecarQuery, response::BeaconVersionedResponse},
 };
 
@@ -30,30 +26,27 @@ pub async fn get_blob_sidecars(
         .clone()
         .unwrap_or((0..beacon_block.message.body.blob_kzg_commitments.len() as u64).collect());
 
-    let mut blob_sidecars = vec![];
+    let (blobs, blob_kzg_proofs) = {
+        let mut blobs = vec![];
+        let mut blob_kzg_proofs = vec![];
 
-    for index in indices {
-        let blob_and_proof = db
-            .blobs_and_proofs_provider()
-            .get(BlobIdentifier { block_root, index })
-            .map_err(|err| {
-                error!("Failed to get blob sidecar, error: {err:?}");
-                ApiError::InternalError
-            })?
-            .ok_or(ApiError::InternalError)?;
-        blob_sidecars.push(BlobSidecar {
-            index,
-            blob: blob_and_proof.blob,
-            kzg_proof: blob_and_proof.proof,
-            kzg_commitment: beacon_block.message.body.blob_kzg_commitments[index as usize],
-            signed_block_header: get_header_from_slot(beacon_block.message.slot, &db)
-                .await?
-                .0,
-            kzg_commitment_inclusion_proof: FixedVector::default(), /* todo: implement kzg
-                                                                     * commitment inclusion
-                                                                     * proofs */
-        });
-    }
+        for index in indices {
+            let blob_and_proof = db
+                .blobs_and_proofs_provider()
+                .get(BlobIdentifier::new(block_root, index))
+                .map_err(|err| {
+                    error!("Failed to get blob sidecar, error: {err:?}");
+                    ApiError::InternalError
+                })?
+                .ok_or(ApiError::InternalError)?;
+            blobs.push(blob_and_proof.blob);
+            blob_kzg_proofs.push(blob_and_proof.proof);
+        }
+        (blobs, blob_kzg_proofs)
+    };
+
+    let blob_sidecars =
+        ream_consensus::blob_sidecar::get_blob_sidecars(beacon_block, blobs, blob_kzg_proofs);
 
     Ok(HttpResponse::Ok().json(BeaconVersionedResponse::new(blob_sidecars)))
 }
