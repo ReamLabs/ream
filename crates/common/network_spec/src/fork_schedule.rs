5a5b16abef0dad95aae29ad919fc20bd253fe061
Lattawit Kulanuwat <sususa003@gmail.com>
2025-04-27 01:44:40 +0700

    fix: remove fork schedules prior to Electra

--- crates/common/network_spec/src/fork_schedule.rs
+++ crates/common/network_spec/src/fork_schedule.rs
@@ -45,7 +45,7 @@ macro_rules! fork_array {
 pub struct ForkSchedule(pub [Fork; ForkSchedule::TOTAL]);
 
 impl ForkSchedule {
-    pub const TOTAL: usize = 6;
+    pub const TOTAL: usize = 1;
 
     pub const fn new(forks: [Fork; ForkSchedule::TOTAL]) -> Self {
         Self(forks)
@@ -62,47 +62,22 @@ impl ForkSchedule {
 }
 
 pub const MAINNET_FORK_SCHEDULE: ForkSchedule = ForkSchedule::new(fork_array!(
-    ("0x00000000", 0),       // Phase0
-    ("0x01000000", 74_240),  // Altair
-    ("0x02000000", 144_896), // Bellatrix
-    ("0x03000000", 194_048), // Capella
-    ("0x04000000", 269_568), // Deneb
     ("0x05000000", 364_032), // Electra
 ));
 
 pub const HOLESKY_FORK_SCHEDULE: ForkSchedule = ForkSchedule::new(fork_array!(
-    ("0x01017000", 0),       // Phase0
-    ("0x02017000", 0),       // Altair
-    ("0x03017000", 0),       // Bellatrix
-    ("0x04017000", 256),     // Capella
-    ("0x05017000", 29_696),  // Deneb
     ("0x06017000", 115_968), // Electra
 ));
 
 pub const SEPOLIA_FORK_SCHEDULE: ForkSchedule = ForkSchedule::new(fork_array!(
-    ("0x90000069", 0),       // Phase0
-    ("0x90000070", 50),      // Altair
-    ("0x90000071", 100),     // Bellatrix
-    ("0x90000072", 56_832),  // Capella
-    ("0x90000073", 132_608), // Deneb
     ("0x90000074", 222_464), // Electra
 ));
 
 pub const HOODI_FORK_SCHEDULE: ForkSchedule = ForkSchedule::new(fork_array!(
-    ("0x10000910", 0),     // Phase0
-    ("0x20000910", 0),     // Altair
-    ("0x30000910", 0),     // Bellatrix
-    ("0x40000910", 0),     // Capella
-    ("0x50000910", 0),     // Deneb
     ("0x60000910", 2_048), // Electra
 ));
 
 pub const DEV_FORK_SCHEDULE: ForkSchedule = ForkSchedule::new(fork_array!(
-    ("0x00000000", 0),       // Phase0
-    ("0x01000000", 74_240),  // Altair
-    ("0x02000000", 144_896), // Bellatrix
-    ("0x03000000", 194_048), // Capella
-    ("0x04000000", 269_568), // Deneb
     ("0x05000000", 364_032), // Electra
 ));
 
