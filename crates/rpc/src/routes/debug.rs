be866ca800ab87208af34af078ded4bf10ac6143
Yuttakhan B <yuttakhanbaingen@gmail.com>
2025-04-27 21:42:36 +0700

    fix: fix incorrect assigned handler

--- crates/rpc/src/routes/debug.rs
+++ crates/rpc/src/routes/debug.rs
@@ -1,9 +1,9 @@
 use actix_web::web::{ServiceConfig, scope};
 
-use crate::handlers::state::get_beacon_state;
+use crate::handlers::{block::get_beacon_heads, state::get_beacon_state};
 
 pub fn register_debug_routes_v1(cfg: &mut ServiceConfig) {
-    cfg.service(scope("/debug").service(get_beacon_state));
+    cfg.service(scope("/debug").service(get_beacon_heads));
 }
 
 pub fn register_debug_routes_v2(cfg: &mut ServiceConfig) {
