bdbe063709d7071d858736baeb6092d6e796aac5
Shariq Naiyer <shariqnaiyer@gmail.com>
2025-05-19 22:42:27 -0600

    fix: remove aggregage_and_proof from this pr

--- crates/common/validator/src/aggregate_and_proof.rs
+++
@@ -1,8 +0,0 @@
-use ream_bls::BLSSignature;
-use ream_consensus::attestation::Attestation;
-
-pub struct AggregateAndProof {
-    pub aggregator_index: u64,
-    pub aggregate: Attestation,
-    pub selection_proof: BLSSignature,
-}
--- crates/common/validator/src/lib.rs
+++ crates/common/validator/src/lib.rs
@@ -1,4 +1,3 @@
-pub mod aggregate_and_proof;
 pub mod constants;
 pub mod execution_requests;
 pub mod validator;
