19504dde2ccfb8756501a6e10067aa5700d5e4c2
Yuttakhan B <yuttakhanbaingen@gmail.com>
2025-05-21 18:08:00 +0700

    refactor: rename method

--- crates/rpc/src/routes/debug.rs
+++ crates/rpc/src/routes/debug.rs
@@ -2,7 +2,7 @@ use actix_web::web::{ServiceConfig, scope};
 
 use crate::handlers::{block::get_beacon_heads, state::get_beacon_state};
 
-pub fn register_debug_routes_v1(cfg: &mut ServiceConfig) {
+pub fn register_debug_routes(cfg: &mut ServiceConfig) {
     cfg.service(scope("/debug").service(get_beacon_heads));
 }
 
