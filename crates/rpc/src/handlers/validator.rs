a52018f41aa67df2d9327aeaddd45406d518f196
bomanaps <mercy.boma35@yahoo.com>
2025-05-23 11:00:55 +0100

    WIP: Updates to discv5 and RPC handler for validator sync committee

--- crates/rpc/src/handlers/validator.rs
+++ crates/rpc/src/handlers/validator.rs
@@ -12,7 +12,7 @@ use ream_discv5::subnet::SyncCommitteeSubnets;
 use ream_storage::db::ReamDB;
 use serde::{Deserialize, Serialize};
 use tokio::sync::RwLock;
-use tracing::error;
+use tracing::{error, info};
 
 use super::state::get_state_from_id;
 use crate::types::{
@@ -359,6 +359,7 @@ pub async fn process_sync_committee_subscriptions(
             }
         }
     }
+    info!("Sync committee subnet subscriptions processed successfully");
     Ok(())
 }
 
