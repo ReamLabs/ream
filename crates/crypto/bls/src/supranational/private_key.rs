47c2e4d3ac700cc47c5c39bd38fdfdd76d24d46e
Shariq Naiyer <shariqnaiyer@gmail.com>
2025-05-24 00:11:19 -0600

    fix: variable naming and update readme

--- crates/crypto/bls/src/supranational/private_key.rs
+++ crates/crypto/bls/src/supranational/private_key.rs
@@ -12,9 +12,9 @@ use crate::{
 impl Signable for PrivateKey {
     type Error = anyhow::Error;
     fn sign(&self, message: &[u8]) -> Result<BLSSignature, Self::Error> {
-        let blst_key = BlstSecretKey::from_bytes(self.inner.as_slice())
+        let private_key = BlstSecretKey::from_bytes(self.inner.as_slice())
             .map_err(|err| anyhow!("Failed to convert to BlstSecretKey: {err:?}"))?;
-        let signature = blst_key.sign(message, DST, &[]);
+        let signature = private_key.sign(message, DST, &[]);
         Ok(BLSSignature {
             inner: FixedVector::new(signature.serialize().to_vec())
                 .map_err(|err| anyhow!("Failed to create to BLSSignature: {err:?}"))?,
