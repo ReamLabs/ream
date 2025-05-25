497965d5bff8449d57d754a807a06357affd03b9
Kayden-ML <kaydenmoroz82@gmail.com>
2025-05-24 21:04:12 -0600

    feat: implement gossip topic sync_committee_contribution_and_proof

---
+++ crates/common/validator/src/contribution_and_proof.rs
@@ -0,0 +1,23 @@
+use alloy_primitives::B256;
+use ream_bls::BLSSignature;
+use serde::{Deserialize, Serialize};
+use ssz_derive::{Decode, Encode};
+use ssz_types::{BitVector, typenum::U128};
+use tree_hash_derive::TreeHash;
+
+#[derive(Debug, PartialEq, Eq, Clone, Serialize, Deserialize, Encode, Decode, TreeHash)]
+pub struct SyncCommitteeContribution {
+    pub slot: u64,
+    pub beacon_block_root: B256,
+    pub subcommittee_index: u64,
+    pub aggregation_bits: BitVector<U128>,
+    pub signature: BLSSignature,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Serialize, Deserialize, Encode, Decode, TreeHash)]
+pub struct ContributionAndProof {
+    #[serde(with = "serde_utils::quoted_u64")]
+    pub aggregator_index: u64,
+    pub contribution: SyncCommitteeContribution,
+    pub selection_proof: BLSSignature,
+}
