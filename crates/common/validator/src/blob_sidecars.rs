cb674944f4ba17750b3a3ea3e889956388e4ae22
Shariq Naiyer <shariqnaiyer@gmail.com>
2025-05-25 10:52:05 -0600

    feat: use network_spec instead of const

--- crates/common/validator/src/blob_sidecars.rs
+++ crates/common/validator/src/blob_sidecars.rs
@@ -1,5 +1,5 @@
-use crate::constants::BLOB_SIDECAR_SUBNET_COUNT_ELECTRA;
+use ream_network_spec::networks::network_spec;
 
 pub fn compute_subnet_for_blob_sidecar(blob_index: u64) -> u64 {
-    blob_index % BLOB_SIDECAR_SUBNET_COUNT_ELECTRA
+    blob_index % network_spec().blob_sidecar_subnet_count_electra
 }
