9acf4950191bc357241feb0537818347f264e7ba
Patchoulis <76623289+Patchoulis@users.noreply.github.com>
2025-05-28 09:57:42 -0600

    feat: added all the other beacon event types

--- crates/common/validator/src/beacon_api_client/event.rs
+++ crates/common/validator/src/beacon_api_client/event.rs
@@ -1,10 +1,25 @@
-use alloy_rpc_types_beacon::events::{BeaconNodeEventTopic, ChainReorgEvent, VoluntaryExitEvent};
+use alloy_rpc_types_beacon::events::{
+    AttestationEvent, BeaconNodeEventTopic, BlobSidecarEvent, BlockEvent,
+    BlsToExecutionChangeEvent, ChainReorgEvent, ContributionAndProofEvent,
+    FinalizedCheckpointEvent, HeadEvent, LightClientFinalityUpdateEvent,
+    LightClientOptimisticUpdateEvent, PayloadAttributesEvent, VoluntaryExitEvent,
+};
 use eventsource_client::Event;
 use serde::de::{DeserializeOwned, Error};
 
 pub enum BeaconEvent {
     ChainReorg(ChainReorgEvent),
     VoluntaryExit(VoluntaryExitEvent),
+    PayloadAttributes(PayloadAttributesEvent),
+    BlobSidecar(BlobSidecarEvent),
+    Block(BlockEvent),
+    BlsToExecutionChange(BlsToExecutionChangeEvent),
+    Head(HeadEvent),
+    LightClientFinalityUpdate(LightClientFinalityUpdateEvent),
+    LightClientOptimisticUpdate(LightClientOptimisticUpdateEvent),
+    ContributionAndProof(ContributionAndProofEvent),
+    FinalizedCheckpoint(FinalizedCheckpointEvent),
+    Attestation(AttestationEvent),
 }
 
 impl BeaconEvent {
@@ -24,6 +39,29 @@ impl TryFrom<Event> for BeaconEvent {
             Self::from_json(&event.data, Self::ChainReorg)
         } else if event.event_type == BeaconNodeEventTopic::VoluntaryExit.query_value() {
             Self::from_json(&event.data, Self::VoluntaryExit)
+        } else if event.event_type == BeaconNodeEventTopic::PayloadAttributes.query_value() {
+            Self::from_json(&event.data, Self::PayloadAttributes)
+        } else if event.event_type == BeaconNodeEventTopic::BlobSidecar.query_value() {
+            Self::from_json(&event.data, Self::BlobSidecar)
+        } else if event.event_type == BeaconNodeEventTopic::Block.query_value() {
+            Self::from_json(&event.data, Self::Block)
+        } else if event.event_type == BeaconNodeEventTopic::BlsToExecutionChange.query_value() {
+            Self::from_json(&event.data, Self::BlsToExecutionChange)
+        } else if event.event_type == BeaconNodeEventTopic::Head.query_value() {
+            Self::from_json(&event.data, Self::Head)
+        } else if event.event_type == BeaconNodeEventTopic::LightClientFinalityUpdate.query_value()
+        {
+            Self::from_json(&event.data, Self::LightClientFinalityUpdate)
+        } else if event.event_type
+            == BeaconNodeEventTopic::LightClientOptimisticUpdate.query_value()
+        {
+            Self::from_json(&event.data, Self::LightClientOptimisticUpdate)
+        } else if event.event_type == BeaconNodeEventTopic::ContributionAndProof.query_value() {
+            Self::from_json(&event.data, Self::ContributionAndProof)
+        } else if event.event_type == BeaconNodeEventTopic::FinalizedCheckpoint.query_value() {
+            Self::from_json(&event.data, Self::FinalizedCheckpoint)
+        } else if event.event_type == BeaconNodeEventTopic::Attestation.query_value() {
+            Self::from_json(&event.data, Self::Attestation)
         } else {
             Err(Self::Error::custom(format!(
                 "Can't create BeaconEvent: unexpected event type: {}",
