1294275598a4291375270510bb2152413b51504e
Patchoulis <76623289+Patchoulis@users.noreply.github.com>
2025-05-30 15:11:36 -0600

    fix: another variable name update

--- crates/crypto/keystore/src/pbkdf2.rs
+++ crates/crypto/keystore/src/pbkdf2.rs
@@ -18,14 +18,14 @@ fn pbkdf2_helper(password: &[u8], salt: &[u8], iterations: u32, index: u32) -> B
 }
 
 pub fn pbkdf2(password: &[u8], salt: &[u8], iterations: u32, derived_key_length: u32) -> Vec<u8> {
-    let num_blocks = (derived_key_length as f64 / 32.0).ceil() as u32;
-    let last_block_size = derived_key_length - (num_blocks - 1) * 32;
+    let block_total = (derived_key_length as f64 / 32.0).ceil() as u32;
+    let last_block_size = derived_key_length - (block_total - 1) * 32;
 
     let mut derived_key = Vec::with_capacity(derived_key_length as usize);
 
-    for block_index in 1..=num_blocks {
+    for block_index in 1..=block_total {
         let block = pbkdf2_helper(password, salt, iterations, block_index);
-        if block_index == num_blocks {
+        if block_index == block_total {
             derived_key.extend_from_slice(&block[..last_block_size as usize]);
         } else {
             derived_key.extend_from_slice(block.as_ref());
