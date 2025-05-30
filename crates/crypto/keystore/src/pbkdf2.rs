9001c735858db91a726b3115d68be278806cfed5
Patchoulis <76623289+Patchoulis@users.noreply.github.com>
2025-05-30 14:49:11 -0600

    fix: updated variable names

--- crates/crypto/keystore/src/pbkdf2.rs
+++ crates/crypto/keystore/src/pbkdf2.rs
@@ -3,30 +3,30 @@ use alloy_primitives::B256;
 use crate::hmac::hmac_sha_256;
 
 // Based on https://www.ietf.org/rfc/rfc2898.txt
-fn pbkdf2_helper(password: &[u8], salt: &[u8], c: u32, i: u32) -> B256 {
-    let mut u = hmac_sha_256(password, &[salt, &i.to_be_bytes()].concat());
-    let mut block = u;
+fn pbkdf2_helper(password: &[u8], salt: &[u8], iterations: u32, index: u32) -> B256 {
+    let mut mac = hmac_sha_256(password, &[salt, &index.to_be_bytes()].concat());
+    let mut block = mac;
 
-    for _ in 1..c {
-        u = hmac_sha_256(password, u.as_ref());
+    for _ in 1..iterations {
+        mac = hmac_sha_256(password, mac.as_ref());
         block
             .iter_mut()
-            .zip(u.iter())
-            .for_each(|(b, u_b)| *b ^= u_b);
+            .zip(mac.iter())
+            .for_each(|(block_byte, mac_byte)| *block_byte ^= mac_byte);
     }
     block
 }
 
-pub fn pbkdf2(password: &[u8], salt: &[u8], c: u32, dk_len: u32) -> Vec<u8> {
-    let l = (dk_len as f64 / 32.0).ceil() as u32;
-    let r = dk_len - (l - 1) * 32;
+pub fn pbkdf2(password: &[u8], salt: &[u8], iterations: u32, dk_len: u32) -> Vec<u8> {
+    let num_blocks = (dk_len as f64 / 32.0).ceil() as u32;
+    let last_block_size = dk_len - (num_blocks - 1) * 32;
 
     let mut derived_key = Vec::with_capacity(dk_len as usize);
 
-    for i in 1..=l {
-        let block = pbkdf2_helper(password, salt, c, i);
-        if i == l {
-            derived_key.extend_from_slice(&block[..r as usize]);
+    for block_index in 1..=num_blocks {
+        let block = pbkdf2_helper(password, salt, iterations, block_index);
+        if block_index == num_blocks {
+            derived_key.extend_from_slice(&block[..last_block_size as usize]);
         } else {
             derived_key.extend_from_slice(block.as_ref());
         }
