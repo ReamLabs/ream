08d7f0823f1497cb824482ea5cbceeb2a9f97b3b
Shariq Naiyer <shariqnaiyer@gmail.com>
2025-05-31 00:06:39 -0600

    fix: bring back get_events_stream

--- crates/common/validator/src/beacon_api_client/mod.rs
+++ crates/common/validator/src/beacon_api_client/mod.rs
@@ -1,14 +1,18 @@
+pub mod event;
 pub mod http_client;
-
-use std::time::Duration;
+use std::{pin::Pin, time::Duration};
 
 use anyhow;
+use event::{BeaconEvent, EventTopic};
+use eventsource_client::{Client, ClientBuilder, SSE};
+use futures::{Stream, StreamExt};
 use http_client::{ClientWithBaseUrl, ContentType};
 use ream_beacon_api_types::{
     duties::ProposerDuty, error::ValidatorError, responses::DutiesResponse,
 };
 use reqwest::Url;
 use ssz::Decode;
+use tracing::{error, info};
 
 pub struct BeaconApiClient {
     pub http_client: ClientWithBaseUrl,
@@ -23,6 +27,50 @@ impl BeaconApiClient {
                 ContentType::Ssz,
             )?,
         })
+    }
+
+    pub fn get_events_stream(
+        &self,
+        topics: &[EventTopic],
+        stream_tag: &'static str,
+    ) -> anyhow::Result<Pin<Box<dyn Stream<Item = BeaconEvent> + Send>>> {
+        let endpoint = self.http_client.base_url().join(&format!(
+            "/eth/v1/events?topics={}",
+            topics
+                .iter()
+                .map(|topic| topic.to_string())
+                .collect::<Vec<_>>()
+                .join(",")
+        ))?;
+
+        Ok(ClientBuilder::for_url(endpoint.as_str())?
+            .build()
+            .stream()
+            .filter_map(move |event| async move {
+                let event = match event {
+                    Ok(SSE::Event(event)) => event,
+                    Ok(SSE::Connected(connection_details)) => {
+                        info!("{stream_tag}: Connected to SSE stream: {connection_details:?}");
+                        return None;
+                    }
+                    Ok(SSE::Comment(comment)) => {
+                        info!("{stream_tag}: Received comment: {comment:?}");
+                        return None;
+                    }
+                    Err(err) => {
+                        error!("{stream_tag}: Error receiving event: {err:?}");
+                        return None;
+                    }
+                };
+                match BeaconEvent::try_from(event) {
+                    Ok(event) => Some(event),
+                    Err(err) => {
+                        error!("{stream_tag}: Failed to decode event: {err:?}");
+                        None
+                    }
+                }
+            })
+            .boxed())
     }
 
     pub async fn get_proposer_duties(
