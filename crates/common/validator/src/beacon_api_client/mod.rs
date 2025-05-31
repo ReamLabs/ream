24f01909c912468ecc2dff80c697daf6760ff75e
Shariq Naiyer <shariqnaiyer@gmail.com>
2025-05-30 23:21:14 -0600

    feat: implement get_proposer_duties and create beacon_api_types crate

--- crates/common/validator/src/beacon_api_client/mod.rs
+++ crates/common/validator/src/beacon_api_client/mod.rs
@@ -1,18 +1,17 @@
-pub mod event;
 pub mod http_client;
 
-use std::{pin::Pin, time::Duration};
+use std::time::Duration;
 
-use event::{BeaconEvent, EventTopic};
-use eventsource_client::{Client, ClientBuilder, SSE};
-use futures::{Stream, StreamExt};
+use anyhow;
 use http_client::{ClientWithBaseUrl, ContentType};
+use ream_beacon_api_types::{
+    duties::ProposerDuty, error::ValidatorError, responses::DutiesResponse,
+};
 use reqwest::Url;
-use tracing::{error, info};
+use ssz::Decode;
 
-#[derive(Clone)]
 pub struct BeaconApiClient {
-    http_client: ClientWithBaseUrl,
+    pub http_client: ClientWithBaseUrl,
 }
 
 impl BeaconApiClient {
@@ -26,47 +25,40 @@ impl BeaconApiClient {
         })
     }
 
-    pub fn get_events_stream(
+    pub async fn get_proposer_duties(
         &self,
-        topics: &[EventTopic],
-        stream_tag: &'static str,
-    ) -> anyhow::Result<Pin<Box<dyn Stream<Item = BeaconEvent> + Send>>> {
-        let endpoint = self.http_client.base_url().join(&format!(
-            "/eth/v1/events?topics={}",
-            topics
-                .iter()
-                .map(|topic| topic.to_string())
-                .collect::<Vec<_>>()
-                .join(",")
-        ))?;
+        epoch: u64,
+    ) -> Result<DutiesResponse<ProposerDuty>, ValidatorError> {
+        let response = self
+            .http_client
+            .execute(
+                self.http_client
+                    .get(format!("eth/v1/validator/duties/proposer/{epoch}"))?
+                    .build()?,
+            )
+            .await?;
 
-        Ok(ClientBuilder::for_url(endpoint.as_str())?
-            .build()
-            .stream()
-            .filter_map(move |event| async move {
-                let event = match event {
-                    Ok(SSE::Event(event)) => event,
-                    Ok(SSE::Connected(connection_details)) => {
-                        info!("{stream_tag}: Connected to SSE stream: {connection_details:?}");
-                        return None;
-                    }
-                    Ok(SSE::Comment(comment)) => {
-                        info!("{stream_tag}: Received comment: {comment:?}");
-                        return None;
-                    }
-                    Err(err) => {
-                        error!("{stream_tag}: Error receiving event: {err:?}");
-                        return None;
-                    }
-                };
-                match BeaconEvent::try_from(event) {
-                    Ok(event) => Some(event),
-                    Err(err) => {
-                        error!("{stream_tag}: Failed to decode event: {err:?}");
-                        None
-                    }
-                }
-            })
-            .boxed())
+        if !response.status().is_success() {
+            return Err(ValidatorError::RequestFailed {
+                status_code: response.status(),
+            });
+        }
+
+        let proposer_duties = if response
+            .headers()
+            .get("content-type")
+            .and_then(|content_type| content_type.to_str().ok())
+            .is_some_and(|content_type| content_type.contains("application/octet-stream"))
+        {
+            DutiesResponse::from_ssz_bytes(&response.bytes().await?)
+                .map_err(|err| ValidatorError::SszDecodeError(format!("{:?}", err)))?
+        } else {
+            response
+                .json()
+                .await
+                .map_err(|err| ValidatorError::JsonDecodeError(err.to_string()))?
+        };
+
+        Ok(proposer_duties)
     }
 }
