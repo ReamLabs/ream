afca1adc0882c0c191c3232ce8b3697dfbeb5237
allnil <nil.medvedev17@gmail.com>
2025-06-02 21:27:51 +0100

    feat: blueprint of syncing endpoint, is_syncing, el_offline, is_optimistic

--- crates/rpc/src/lib.rs
+++ crates/rpc/src/lib.rs
@@ -3,6 +3,8 @@ use std::sync::Arc;
 use actix_web::{App, HttpServer, dev::ServerHandle, middleware, web::Data};
 use config::RpcServerConfig;
 use ream_operation_pool::OperationPool;
+use ream_beacon_chain::beacon_chain::BeaconChain;
+use ream_execution_engine::ExecutionEngine;
 use ream_p2p::network_state::NetworkState;
 use ream_storage::db::ReamDB;
 use tracing::info;
@@ -19,6 +21,7 @@ pub async fn start_server(
     db: ReamDB,
     network_state: Arc<NetworkState>,
     operation_pool: Arc<OperationPool>,
+    execution_engine: ExecutionEngine,
 ) -> std::io::Result<()> {
     info!(
         "starting HTTP server on {:?}",
@@ -35,6 +38,7 @@ pub async fn start_server(
             .app_data(Data::new(db.clone()))
             .app_data(Data::new(network_state.clone()))
             .app_data(Data::new(operation_pool.clone()))
+            .app_data(Data::new(execution_engine.clone()))
             .configure(register_routers)
     })
     .bind(server_config.http_socket_address)?
