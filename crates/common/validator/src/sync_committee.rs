a053989fba8df476132a67a82e6b31dfa63e98f2
Patchoulis <76623289+Patchoulis@users.noreply.github.com>
2025-06-12 23:01:49 -0600

    feat: added the method for sync committees for non-aggregators

--- crates/common/validator/src/sync_committee.rs
+++ crates/common/validator/src/sync_committee.rs
@@ -9,9 +9,10 @@ use ream_bls::{
 use ream_consensus::{
     constants::{DOMAIN_SYNC_COMMITTEE, EPOCHS_PER_SYNC_COMMITTEE_PERIOD, SYNC_COMMITTEE_SIZE},
     electra::{beacon_block::BeaconBlock, beacon_state::BeaconState},
-    misc::{compute_epoch_at_slot, compute_signing_root},
+    misc::{compute_domain, compute_epoch_at_slot, compute_signing_root},
     sync_aggregate::SyncAggregate,
 };
+use ream_network_spec::networks::network_spec;
 use serde::{Deserialize, Serialize};
 use ssz_types::{BitVector, typenum::U512};
 use tree_hash_derive::TreeHash;
@@ -27,8 +28,8 @@ use crate::{
 
 #[derive(Debug, PartialEq, Eq, Clone, Serialize, Deserialize, TreeHash)]
 pub struct SyncAggregatorSelectionData {
-    slot: u64,
-    subcommittee_index: u64,
+    pub slot: u64,
+    pub subcommittee_index: u64,
 }
 
 pub struct SyncCommitteeMessage {
@@ -135,24 +136,14 @@ pub fn process_sync_committee_contributions(
     Ok(())
 }
 
-pub fn get_sync_committee_selection_proof(
-    state: &BeaconState,
-    slot: u64,
-    subcommittee_index: u64,
-    private_key: PrivateKey,
-) -> anyhow::Result<BLSSignature> {
-    let domain = state.get_domain(
-        DOMAIN_SYNC_COMMITTEE_SELECTION_PROOF,
-        Some(compute_epoch_at_slot(slot)),
+pub fn get_sync_committee_selection_proof(slot: u64, subcommittee_index: u64, privkey: &PrivateKey) -> anyhow::Result<BLSSignature> {
+    let domain = compute_domain(
+        DOMAIN_SYNC_COMMITTEE,
+        Some(network_spec().electra_fork_version),
+        None,
     );
-    let signing_root = compute_signing_root(
-        SyncAggregatorSelectionData {
-            slot,
-            subcommittee_index,
-        },
-        domain,
-    );
-    Ok(private_key.sign(signing_root.as_ref())?)
+    let signing_root = compute_signing_root(SyncAggregatorSelectionData {slot, subcommittee_index}, domain);
+    Ok(privkey.sign(signing_root.as_ref())?)
 }
 
 pub fn is_sync_committee_aggregator(signature: BLSSignature) -> bool {
