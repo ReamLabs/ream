b98ca00c4430f3e0c78ee713d84b714b67992f7c
Kayden-ML <kaydenmoroz82@gmail.com>
2025-06-26 15:18:37 -0600

    fix: implemented the positive test

--- crates/common/consensus/src/blob_sidecar.rs
+++ crates/common/consensus/src/blob_sidecar.rs
@@ -57,17 +57,57 @@ impl BlobSidecar {
 mod tests {
     use alloy_primitives::B256;
     use ream_bls::BLSSignature;
+    use ream_merkle::{generate_proof, merkle_tree};
     use ssz_types::{FixedVector, typenum::U17};
+    use tree_hash::TreeHash;
 
     use crate::{
         beacon_block_header::{BeaconBlockHeader, SignedBeaconBlockHeader},
         blob_sidecar::BlobSidecar,
+        constants::BLOB_KZG_COMMITMENTS_INDEX,
         execution_engine::rpc_types::get_blobs::Blob,
         polynomial_commitments::{kzg_commitment::KZGCommitment, kzg_proof::KZGProof},
     };
 
     #[test]
-    fn verify_blob_sidecar_inclusion_proof() -> anyhow::Result<()> {
+    fn verify_blob_sidecar_inclusion_proof_positive() -> anyhow::Result<()> {
+        let blob = Blob {
+            inner: FixedVector::from(vec![0x42; 131072]),
+        };
+
+        let kzg_commitment = KZGCommitment([0u8; 48]);
+        let depth = 17;
+
+        let mut leaves = vec![B256::default(); 1 << depth];
+        leaves[BLOB_KZG_COMMITMENTS_INDEX as usize] = kzg_commitment.tree_hash_root();
+
+        let tree = merkle_tree(&leaves, depth)?;
+        let root = tree[1];
+
+        let mut proof = generate_proof(&tree, BLOB_KZG_COMMITMENTS_INDEX, depth)?;
+        proof.resize(17, B256::default());
+
+        let blob_sidecar = BlobSidecar {
+            index: BLOB_KZG_COMMITMENTS_INDEX,
+            blob,
+            kzg_commitment,
+            kzg_proof: KZGProof::default(),
+            signed_block_header: SignedBeaconBlockHeader {
+                message: BeaconBlockHeader {
+                    body_root: root,
+                    ..Default::default()
+                },
+                signature: BLSSignature::default(),
+            },
+            kzg_commitment_inclusion_proof: FixedVector::from(proof),
+        };
+
+        assert!(blob_sidecar.verify_blob_sidecar_inclusion_proof());
+        Ok(())
+    }
+
+    #[test]
+    fn verify_blob_sidecar_inclusion_proof_negative() -> anyhow::Result<()> {
         let signed_block_header = SignedBeaconBlockHeader {
             message: BeaconBlockHeader::default(),
             signature: BLSSignature::default(),
