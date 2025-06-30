0190c9afd3edad46e19f17bc8d4b43d18063aba5
varun-doshi <doshivarun202@gmail.com>
2025-06-30 17:36:39 +0530

    fix: remove unwrap

--- crates/storage/src/cache.rs
+++ crates/storage/src/cache.rs
@@ -5,7 +5,7 @@ use ream_bls::{BLSSignature, PublicKey};
 use ream_consensus::bls_to_execution_change::BLSToExecutionChange;
 use tokio::sync::RwLock;
 
-/// In-memory cache.
+/// In-memory LRU cache.
 #[derive(Debug)]
 pub struct CachedDB {
     pub cached_proposer_signature: RwLock<LruCache<(PublicKey, u64), BLSSignature>>,
@@ -15,8 +15,12 @@ pub struct CachedDB {
 impl CachedDB {
     pub fn new() -> Self {
         Self {
-            cached_proposer_signature: LruCache::new(NonZeroUsize::new(64).unwrap()).into(),
-            cached_bls_to_execution_signature: LruCache::new(NonZeroUsize::new(64).unwrap()).into(),
+            cached_proposer_signature: LruCache::new(unsafe { NonZeroUsize::new_unchecked(64) })
+                .into(),
+            cached_bls_to_execution_signature: LruCache::new(unsafe {
+                NonZeroUsize::new_unchecked(64)
+            })
+            .into(),
         }
     }
 }
