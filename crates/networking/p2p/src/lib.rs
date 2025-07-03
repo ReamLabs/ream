48dba72e54f2992de42d51b7c8f00f8de2eb3a83
varun-doshi <doshivarun202@gmail.com>
2025-07-03 16:21:29 +0530

    feat: expose local node info

--- crates/networking/p2p/src/lib.rs
+++ crates/networking/p2p/src/lib.rs
@@ -5,6 +5,7 @@ pub mod constants;
 pub mod gossipsub;
 pub mod network;
 pub mod network_state;
+pub mod node;
 pub mod peer;
 pub mod req_resp;
 pub mod utils;
--- crates/networking/p2p/src/req_resp/messages/meta_data.rs
+++ crates/networking/p2p/src/req_resp/messages/meta_data.rs
@@ -1,9 +1,10 @@
+use serde::Serialize;
 use ssz_derive::{Decode, Encode};
 use ssz_types::BitVector;
 
 use crate::req_resp::configurations::{AttestationSubnetCount, SyncCommitteeSubnetCount};
 
-#[derive(Debug, Default, Clone, PartialEq, Eq, Encode, Decode)]
+#[derive(Debug, Default, Clone, PartialEq, Eq, Encode, Decode, Serialize)]
 pub struct GetMetaDataV2 {
     pub seq_number: u64,
     pub attnets: BitVector<AttestationSubnetCount>,
