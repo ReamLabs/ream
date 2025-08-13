7a9f94414ac4281a05be9fad3b955c3d237d56f8
varun-doshi <doshivarun202@gmail.com>
2025-08-13 20:33:56 +0530

    fix: remove StopHandle

--- crates/rpc/lean/src/lib.rs
+++ crates/rpc/lean/src/lib.rs
@@ -1,6 +1,6 @@
 use std::sync::Arc;
 
-use actix_web::{App, HttpServer, dev::ServerHandle, middleware, web::Data};
+use actix_web::{App, HttpServer, middleware, web::Data};
 use config::LeanRpcServerConfig;
 use ream_chain_lean::lean_chain::LeanChain;
 use tokio::sync::RwLock;
@@ -21,13 +21,10 @@ pub async fn start_lean_server(
         "starting HTTP server on {:?}",
         server_config.http_socket_address
     );
-    let stop_handle = Data::new(StopHandle::default());
 
     let server = HttpServer::new(move || {
-        let stop_handle = stop_handle.clone();
         App::new()
             .wrap(middleware::Logger::default())
-            .app_data(stop_handle)
             .app_data(Data::new(lean_chain.clone()))
             .configure(register_routers)
     })
@@ -35,21 +32,4 @@ pub async fn start_lean_server(
     .run();
 
     server.await
-}
-
-#[derive(Default)]
-struct StopHandle {
-    inner: parking_lot::Mutex<Option<ServerHandle>>,
-}
-
-#[allow(dead_code)]
-impl StopHandle {
-    pub(crate) fn register(&self, handle: ServerHandle) {
-        *self.inner.lock() = Some(handle);
-    }
-
-    pub(crate) fn stop(&self, graceful: bool) {
-        #[allow(clippy::let_underscore_future)]
-        let _ = self.inner.lock().as_ref().unwrap().stop(graceful);
-    }
 }
