ad7972cb2c90e08baa0300e9996b29bd2e71b572
varun-doshi <doshivarun202@gmail.com>
2025-09-26 11:31:46 +0900

    fix: refactor

--- crates/rpc/lean/src/handlers/state.rs
+++ crates/rpc/lean/src/handlers/state.rs
@@ -5,49 +5,43 @@ use actix_web::{
 use ream_api_types_common::{error::ApiError, id::ID};
 use ream_chain_lean::lean_chain::LeanChainReader;
 use ream_consensus_lean::state::LeanState;
-use tree_hash::TreeHash;
+use ream_storage::tables::table::Table;
 
-// GET /lean/v0/states/{block_id}
-#[get("/states/{block_id}")]
+// GET /lean/v0/states/{state_id}
+#[get("/states/{state_id}")]
 pub async fn get_state(
     block_id: Path<ID>,
     lean_chain: Data<LeanChainReader>,
 ) -> Result<impl Responder, ApiError> {
-    Ok(HttpResponse::Ok().json(
-        get_state_by_block_id(block_id.into_inner(), lean_chain)
-            .await?
-            .ok_or_else(|| ApiError::NotFound("Block not found".to_string()))?,
-    ))
+    Ok(HttpResponse::Ok().json(get_state_from_id(block_id.into_inner(), lean_chain).await?))
 }
 
-// Retrieve a state from the lean chain by its block ID.
-pub async fn get_state_by_block_id(
+// Retrieve a state from the lean chain by its state ID.
+pub async fn get_state_from_id(
     block_id: ID,
     lean_chain: Data<LeanChainReader>,
-) -> Result<Option<LeanState>, ApiError> {
-    // Obtain read guard first from the reader.
+) -> Result<LeanState, ApiError> {
     let lean_chain = lean_chain.read().await;
 
-    Ok(match block_id {
-        ID::Finalized => {
-            lean_chain.get_state_by_block_hash(lean_chain.latest_finalized_hash().ok_or(
-                ApiError::InternalError("Failed to get latest finalized hash".to_string()),
-            )?)
-        }
-        ID::Genesis => lean_chain.get_state_by_block_hash(lean_chain.genesis_hash),
-        ID::Head => lean_chain.get_state_by_block_hash(lean_chain.head),
-        ID::Justified => {
-            lean_chain.get_state_by_block_hash(lean_chain.latest_justified_hash().ok_or(
-                ApiError::InternalError("Failed to get latest justified hash".to_string()),
-            )?)
-        }
-        ID::Slot(slot) => {
-            let block = lean_chain
-                .get_block_by_slot(slot)
-                .ok_or(ApiError::NotFound("Block not found".to_string()))?;
+    let block_root =
+        match block_id {
+            ID::Finalized => lean_chain.latest_finalized_hash().await.map_err(|err| {
+                ApiError::InternalError(format!("No latest finalized hash: {err:?}"))
+            }),
+            ID::Genesis => Ok(lean_chain.genesis_hash),
+            ID::Head => Ok(lean_chain.head),
+            ID::Justified => lean_chain.latest_justified_hash().await.map_err(|err| {
+                ApiError::InternalError(format!("No latest justified hash: {err:?}"))
+            }),
+            ID::Slot(slot) => lean_chain.get_block_id_by_slot(slot).await.map_err(|err| {
+                ApiError::InternalError(format!("No block for slot {slot}: {err:?}"))
+            }),
+            ID::Root(root) => Ok(root),
+        };
 
-            lean_chain.get_state_by_block_hash(block.tree_hash_root())
-        }
-        ID::Root(root) => lean_chain.get_state_by_block_hash(root),
-    })
+    let provider = lean_chain.store.clone().lock().await.lean_state_provider();
+    provider
+        .get(block_root?)
+        .map_err(|err| ApiError::InternalError(format!("DB error: {err}")))?
+        .ok_or_else(|| ApiError::NotFound("Lean state not found".to_string()))
 }
