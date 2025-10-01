2b48ec5d11c52c42a0d6beaf977f1539e9371cb0
Shariq Naiyer <shariqnaiyer@gmail.com>
2025-09-30 21:52:23 -0600

    feat: add initial variables

--- testing/lean-tests/tests/state.rs
+++ testing/lean-tests/tests/state.rs
@@ -1,5 +1,14 @@
+use ream_consensus_lean::{config::Config, state::LeanState};
+use ream_consensus_misc::constants::lean::VALIDATOR_REGISTRY_LIMIT;
+
 #[test]
 fn test_placeholder() {
-    // Add your tests here
-    assert_eq!(1, 1);
+    let sample_config = Config {
+        num_validators: VALIDATOR_REGISTRY_LIMIT,
+        genesis_time: 0,
+    };
+
+    let genesis_state = LeanState::new(VALIDATOR_REGISTRY_LIMIT, 0);
+
+    
 }
