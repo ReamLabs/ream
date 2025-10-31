8a701ae64ea515e86af6c5db82dd5b7bbaabed02
Shariq Naiyer <shariqnaiyer@gmail.com>
2025-10-30 22:48:28 -0600

    fix: order

--- crates/crypto/post_quantum/src/hashsig/mod.rs
+++ crates/crypto/post_quantum/src/hashsig/mod.rs
@@ -3,11 +3,11 @@ pub mod private_key;
 pub mod public_key;
 pub mod signature;
 
-#[cfg(test)]
-pub type HashSigScheme = hashsig::signature::generalized_xmss::instantiations_poseidon_top_level::lifetime_2_to_the_8::SIGTopLevelTargetSumLifetime8Dim64Base8;
-
 #[cfg(all(not(test), feature = "signature-scheme-prod"))]
 pub type HashSigScheme = hashsig::signature::generalized_xmss::instantiations_poseidon_top_level::lifetime_2_to_the_32::hashing_optimized::SIGTopLevelTargetSumLifetime32Dim64Base8;
 
 #[cfg(all(not(test), feature = "signature-scheme-test"))]
+pub type HashSigScheme = hashsig::signature::generalized_xmss::instantiations_poseidon_top_level::lifetime_2_to_the_8::SIGTopLevelTargetSumLifetime8Dim64Base8;
+
+#[cfg(test)]
 pub type HashSigScheme = hashsig::signature::generalized_xmss::instantiations_poseidon_top_level::lifetime_2_to_the_8::SIGTopLevelTargetSumLifetime8Dim64Base8;
