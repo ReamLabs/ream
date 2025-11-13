6ae7a3531f701f7b054b551d01abc716462c8577
varun-doshi <doshivarun202@gmail.com>
2025-11-13 13:40:20 +0530

    feat: use bytes in signature container

--- crates/crypto/post_quantum/src/hashsig/errors.rs
+++ crates/crypto/post_quantum/src/hashsig/errors.rs
@@ -1,5 +1,14 @@
 #[derive(Debug, thiserror::Error)]
-pub enum SigningError {
+pub enum SignatureError {
     #[error("Signing failed: {0:?}")]
     SigningFailed(hashsig::signature::SigningError),
+
+    #[error("Signature serialization failed")]
+    SerializationFailed,
+
+    #[error("Invalid signature length")]
+    InvalidSignatureLength,
+
+    #[error("Signature decode failed: {0:?}")]
+    SignatureDecodeFailed(bincode::error::DecodeError),
 }
