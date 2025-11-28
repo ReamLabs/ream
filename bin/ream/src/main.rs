8562b0d3c0ba6e62e3d3d7a9c331654bc38af3c2
mw2000 <mwadekar2000@gmail.com>
2025-11-27 17:05:37 -0800

    feat: implements beacon chain events endpoint
    We implement the events endpoint for beacon chain to be able to send events over SSE.

--- bin/ream/src/main.rs
+++ bin/ream/src/main.rs
@@ -25,7 +25,7 @@ use ream::cli::{
     voluntary_exit::VoluntaryExitConfig,
 };
 use ream_account_manager::{message_types::MessageType, seed::derive_seed_with_user_input};
-use ream_api_types_beacon::id::ValidatorID;
+use ream_api_types_beacon::{events::BeaconEvent, id::ValidatorID};
 use ream_api_types_common::id::ID;
 use ream_chain_lean::{
     genesis as lean_genesis, messages::LeanChainServiceMessage, p2p_request::LeanP2PRequest,
@@ -74,7 +74,10 @@ use ream_validator_lean::{
     registry::load_validator_registry, service::ValidatorService as LeanValidatorService,
 };
 use ssz_types::VariableList;
-use tokio::{sync::mpsc, time::Instant};
+use tokio::{
+    sync::{broadcast, mpsc},
+    time::Instant,
+};
 use tracing::{error, info};
 use tracing_subscriber::EnvFilter;
 
@@ -347,6 +350,8 @@ pub async fn run_beacon_node(config: BeaconNodeConfig, executor: ReamExecutor, r
 
     let operation_pool = Arc::new(OperationPool::default());
 
+    let (event_sender, _) = broadcast::channel::<BeaconEvent>(1024);
+
     let server_config = RpcServerConfig::new(
         config.http_address,
         config.http_port,
@@ -359,6 +364,7 @@ pub async fn run_beacon_node(config: BeaconNodeConfig, executor: ReamExecutor, r
         beacon_db.clone(),
         beacon_db.data_dir.clone(),
         operation_pool.clone(),
+        event_sender.clone(),
     )
     .await
     .expect("Failed to create manager service");
@@ -378,6 +384,7 @@ pub async fn run_beacon_node(config: BeaconNodeConfig, executor: ReamExecutor, r
             network_state,
             operation_pool,
             execution_engine,
+            event_sender,
         )
         .await
     });
--- crates/networking/manager/src/service.rs
+++ crates/networking/manager/src/service.rs
@@ -59,6 +59,7 @@ impl NetworkManagerService {
         ream_db: BeaconDB,
         ream_dir: PathBuf,
         operation_pool: Arc<OperationPool>,
+        event_sender: tokio::sync::broadcast::Sender<ream_api_types_beacon::events::BeaconEvent>,
     ) -> anyhow::Result<Self> {
         let discv5_config = discv5::ConfigBuilder::new(discv5::ListenConfig::from_ip(
             config.socket_address,
@@ -102,6 +103,7 @@ impl NetworkManagerService {
             ream_db.clone(),
             operation_pool,
             execution_engine,
+            Some(event_sender),
         ));
         let status = beacon_chain.build_status_request().await?;
 
