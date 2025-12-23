9a9f53e502ba871b2d75a2e1f333529c69379867
Abhivansh <31abhivanshj@gmail.com>
2025-12-23 21:35:41 +0530

    recover matrix fn done

--- crates/common/consensus/beacon/src/matrix_entry.rs
+++ crates/common/consensus/beacon/src/matrix_entry.rs
@@ -1,7 +1,7 @@
-use anyhow::{Result, anyhow};
+use anyhow::{Ok, Result, anyhow};
 use ream_consensus_misc::polynomial_commitments::kzg_proof::KZGProof;
 use ream_execution_rpc_types::get_blobs::Blob;
-use rust_eth_kzg::{Cell as KZGCell, DASContext, KZGProof as Proof, TrustedSetup};
+use rust_eth_kzg::{Cell as KZGCell, DASContext, KZGProof as Proof};
 use ssz_types::FixedVector;
 
 use crate::data_column_sidecar::Cell;
@@ -19,13 +19,51 @@ pub fn compute_matrix(blobs: Vec<Blob>, das_context: &DASContext) -> Result<Vec<
 
     for (blob_index, blob) in blobs.iter().enumerate() {
         let (cells, proofs) = compute_cells_and_kzg_proofs(blob, das_context)?;
-        // For each column/row in the blob/cells, create a MatrixEntry.
         for (cell_index, (cell, kzg_proof)) in cells.into_iter().zip(proofs.into_iter()).enumerate()
         {
             matrix.push(MatrixEntry {
                 cell,
                 kzg_proof,
                 column_index: blob_index as u64,
+                row_index: cell_index as u64,
+            });
+        }
+    }
+
+    Ok(matrix)
+}
+
+pub fn remove_matrix(
+    partial_matrix: Vec<MatrixEntry>,
+    blob_count: u64,
+    das_context: &DASContext,
+) -> Result<Vec<MatrixEntry>> {
+    let mut matrix: Vec<MatrixEntry> = Vec::new();
+
+    for blob_index in 0..blob_count {
+        let cell_indices: Vec<u64> = partial_matrix
+            .iter()
+            .filter(|e| e.row_index == blob_index)
+            .map(|e| e.column_index)
+            .collect();
+        let cells: Vec<Cell> = partial_matrix
+            .iter()
+            .filter(|e| e.row_index == blob_index)
+            .map(|e| e.cell.clone())
+            .collect();
+
+        let (recovered_cells, recovered_proofs) =
+            recover_cells_and_kzg_proofs(cell_indices, cells, das_context)?;
+
+        for (cell_index, (cell, kzg_proof)) in recovered_cells
+            .into_iter()
+            .zip(recovered_proofs.into_iter())
+            .enumerate()
+        {
+            matrix.push(MatrixEntry {
+                cell,
+                kzg_proof,
+                column_index: blob_index,
                 row_index: cell_index as u64,
             });
         }
@@ -55,6 +93,37 @@ fn compute_cells_and_kzg_proofs(
 
     let cells: Vec<Cell> = kzg_cells.into_iter().map(convert_cell).collect();
     let proofs: Vec<KZGProof> = kzg_proofs.into_iter().map(convert_kzg_proof).collect();
+
+    Ok((cells, proofs))
+}
+
+fn recover_cells_and_kzg_proofs(
+    cell_indices: Vec<u64>,
+    cells: Vec<Cell>,
+    das_context: &DASContext,
+) -> Result<(Vec<Cell>, Vec<KZGProof>)> {
+    let kzg_cells: Result<Vec<[u8; 2048]>> = cells
+        .into_iter()
+        .map(|fv| {
+            let vec: Vec<u8> = fv.into();
+            if vec.len() != 2048 {
+                return Err(anyhow!("Cell length {}, expected 2048", vec.len()));
+            }
+            let arr: [u8; 2048] = vec
+                .try_into()
+                .map_err(|err| anyhow!("Failed to convert Cell to [u8; 2048]: {:?}", err))?;
+            Ok(arr)
+        })
+        .collect();
+
+    let kzg_cells = kzg_cells?;
+    let kzg_cells_refs: Vec<&[u8; 2048]> = kzg_cells.iter().collect();
+    let (new_kzg_cells, new_kzg_proofs) = das_context
+        .recover_cells_and_kzg_proofs(cell_indices, kzg_cells_refs)
+        .map_err(|err| anyhow!("KZG recovery error: {:?}", err))?;
+
+    let cells: Vec<Cell> = new_kzg_cells.into_iter().map(convert_cell).collect();
+    let proofs: Vec<KZGProof> = new_kzg_proofs.into_iter().map(convert_kzg_proof).collect();
 
     Ok((cells, proofs))
 }
