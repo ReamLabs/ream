42a4b837623fdce3e42e282690abfe8a64b96e3d
mw2000 <mwadekar2000@gmail.com>
2025-12-23 00:30:48 -0800

    feat: implements /eth/v1/validator/sync_committee_subscriptions
    We implement the sync comittee subscriptions endpoint.

--- crates/common/api_types/beacon/src/committee.rs
+++ crates/common/api_types/beacon/src/committee.rs
@@ -12,3 +12,13 @@ pub struct BeaconCommitteeSubscription {
     pub slot: u64,
     pub is_aggregator: bool,
 }
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct SyncCommitteeSubscription {
+    #[serde(with = "serde_utils::quoted_u64")]
+    pub validator_index: u64,
+    #[serde(with = "serde_utils::quoted_u64_vec")]
+    pub sync_committee_indices: Vec<u64>,
+    #[serde(with = "serde_utils::quoted_u64")]
+    pub until_epoch: u64,
+}
