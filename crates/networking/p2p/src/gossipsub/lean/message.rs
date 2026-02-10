a34521e774a1e32165e95f26cda500e3cb580280
Shariq Naiyer <shariqnaiyer@gmail.com>
2026-02-09 17:36:54 -0700

    feat: add gossip handling of aggregated attestation (#1214)
    * feat: add gossip handling of aggregated attestation
    
    * fix: inline

--- crates/networking/p2p/src/gossipsub/lean/message.rs
+++ crates/networking/p2p/src/gossipsub/lean/message.rs
@@ -1,4 +1,6 @@
 use libp2p::gossipsub::TopicHash;
+#[cfg(feature = "devnet3")]
+use ream_consensus_lean::attestation::SignedAggregatedAttestation;
 use ream_consensus_lean::{attestation::SignedAttestation, block::SignedBlockWithAttestation};
 use ssz::Decode;
 
@@ -8,7 +10,15 @@ use crate::gossipsub::error::GossipsubError;
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum LeanGossipsubMessage {
     Block(Box<SignedBlockWithAttestation>),
+    #[cfg(feature = "devnet2")]
     Attestation(Box<SignedAttestation>),
+    #[cfg(feature = "devnet3")]
+    Attestation {
+        subnet_id: u64,
+        attestation: Box<SignedAttestation>,
+    },
+    #[cfg(feature = "devnet3")]
+    AggregatedAttestation(Box<SignedAggregatedAttestation>),
 }
 
 impl LeanGossipsubMessage {
@@ -17,9 +27,19 @@ impl LeanGossipsubMessage {
             LeanGossipTopicKind::Block => Ok(Self::Block(Box::new(
                 SignedBlockWithAttestation::from_ssz_bytes(data)?,
             ))),
+            #[cfg(feature = "devnet2")]
             LeanGossipTopicKind::Attestation => Ok(Self::Attestation(Box::new(
                 SignedAttestation::from_ssz_bytes(data)?,
             ))),
+            #[cfg(feature = "devnet3")]
+            LeanGossipTopicKind::AttestationSubnet(subnet_id) => Ok(Self::Attestation {
+                subnet_id,
+                attestation: Box::new(SignedAttestation::from_ssz_bytes(data)?),
+            }),
+            #[cfg(feature = "devnet3")]
+            LeanGossipTopicKind::AggregatedAttestation => Ok(Self::AggregatedAttestation(
+                Box::new(SignedAggregatedAttestation::from_ssz_bytes(data)?),
+            )),
         }
     }
 }
