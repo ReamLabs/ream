cf8616e7fed6b7ff580babbcdf52e5f29a00f271
Shariq Naiyer <shariqnaiyer@gmail.com>
2026-02-19 20:57:38 -0700

    fix: match intervals

--- crates/common/consensus/misc/src/constants/lean.rs
+++ crates/common/consensus/misc/src/constants/lean.rs
@@ -1,9 +1,9 @@
 pub const ATTESTATION_COMMITTEE_COUNT: u64 = 1;
 
-#[cfg(feature = "devnet2")]
+#[cfg(all(feature = "devnet2", not(feature = "devnet3")))]
 pub const INTERVALS_PER_SLOT: u64 = 4;
 #[cfg(feature = "devnet3")]
 pub const INTERVALS_PER_SLOT: u64 = 5;
 pub const MAX_HISTORICAL_BLOCK_HASHES: u64 = 262144;
-pub const SLOT_DURATION: u64 = 12;
+pub const SLOT_DURATION: u64 = 4;
 pub const VALIDATOR_REGISTRY_LIMIT: u64 = 4096;
