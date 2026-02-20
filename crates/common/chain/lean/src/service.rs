7d4fd6055a4387de7b04b6b97fd64be2fe5b7119
Shariq Naiyer <shariqnaiyer@gmail.com>
2026-02-19 21:02:17 -0700

    fix: clean

--- crates/common/chain/lean/src/service.rs
+++ crates/common/chain/lean/src/service.rs
@@ -136,7 +136,7 @@ impl LeanChainService {
                         self.store.write().await.tick_interval(tick_count.is_multiple_of(INTERVALS_PER_SLOT)).await.expect("Failed to tick interval");
                         #[cfg(feature = "devnet3")]
                         {
-                            // TODO: update is_aggregator logic from
+                            // TODO: update is_aggregator logic from devnet config
                             let is_aggregator = true;
                             self.store.write().await.tick_interval(tick_count.is_multiple_of(INTERVALS_PER_SLOT), is_aggregator).await.expect("Failed to tick interval");
                         }
