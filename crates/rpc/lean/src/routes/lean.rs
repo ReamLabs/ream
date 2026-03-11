dba18e56158562178250494bf4101c6a0bf95c5f
Shariq Naiyer <shariqnaiyer@gmail.com>
2026-03-10 22:21:03 -0600

    feat: add handler for fork choice

--- crates/rpc/lean/src/routes/lean.rs
+++ crates/rpc/lean/src/routes/lean.rs
@@ -2,7 +2,7 @@ use actix_web::web::ServiceConfig;
 
 use crate::handlers::{
     block::get_block, block_header::get_block_header, checkpoint::get_justified_checkpoint,
-    head::get_head, health::get_health, state::get_state,
+    fork_choice::get_fork_choice_tree, head::get_head, health::get_health, state::get_state,
 };
 
 /// Creates and returns all `/lean` routes.
@@ -10,6 +10,7 @@ pub fn register_lean_routes(cfg: &mut ServiceConfig) {
     cfg.service(get_head)
         .service(get_block)
         .service(get_block_header)
+        .service(get_fork_choice_tree)
         .service(get_justified_checkpoint)
         .service(get_state)
         .service(get_health);
