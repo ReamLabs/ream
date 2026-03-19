d946abdc4d2bd8e7d991a0867269d7539cc0fd6e
Derek Sorken <113465018+Dsorken@users.noreply.github.com>
2026-03-19 12:55:56 -0600

    test: update test_validator_not_found_rejects test (#1311)
    update test

--- testing/gossip-validation/tests/validate_block.rs
+++ testing/gossip-validation/tests/validate_block.rs
@@ -208,15 +208,13 @@ mod tests {
         );
     }
 
-    /// TODO: Update test to Fulu
-    #[ignore = "Update test to Fulu"]
     #[tokio::test]
     pub async fn test_validator_not_found_rejects() {
         initialize_test_network_spec();
         let (beacon_chain, cached_db, _block_root) = db_setup().await;
 
         let mut incoming_beacon_block = read_ssz_snappy_file::<SignedBeaconBlock>(
-            "./assets/sepolia/blocks/slot_8084250.ssz_snappy",
+            "./assets/sepolia/blocks/child_9552076.ssz_snappy",
         )
         .unwrap();
 
