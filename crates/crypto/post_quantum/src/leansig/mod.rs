7e371d415d3149d054c1eb30c690fd418925dd09
Shariq Naiyer <shariqnaiyer@gmail.com>
2026-04-26 17:34:39 -0600

    feat: update leanmultisig (#1355)

--- crates/crypto/post_quantum/src/leansig/mod.rs
+++ crates/crypto/post_quantum/src/leansig/mod.rs
@@ -3,4 +3,4 @@ pub mod private_key;
 pub mod public_key;
 pub mod signature;
 
-pub type LeanSigScheme = leansig::signature::generalized_xmss::instantiations_aborting::lifetime_2_to_the_32::SchemeAbortingTargetSumLifetime32Dim46Base8;
+pub type LeanSigScheme = leansig::signature::generalized_xmss::instantiations_aborting::lifetime_2_to_the_32::SIGAbortingTargetSumLifetime32Dim46Base8;
